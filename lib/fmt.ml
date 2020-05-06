9294578badb0600b1bf9b59848cc5a0362908a7d
Calascibetta Romain <romain.calascibetta@gmail.com>
2020-05-06 16:09:22 +0200

    Keep compatibility with 4.06

--- lib/fmt.ml
+++ lib/fmt.ml
@@ -1,6 +1,7 @@
 (* (c) Daniel Bünzli *)
 
 open Stdlib
+open Fmt_meta
 
 let invalid_arg fmt =
   Format.kasprintf invalid_arg fmt
@@ -13,19 +14,6 @@ let style_renderer_of_raw = function
 let style_renderer_to_raw = function
   | `None -> "\x00"
   | `Ansi -> "\x01"
-
-type Format.stag += Hxd_style_renderer
-let style_renderer_tag = Hxd_style_renderer
-
-let meta_store ppf = Format.pp_get_formatter_stag_functions ppf ()
-let set_meta_store ppf store = Format.pp_set_formatter_stag_functions ppf store
-let meta_raw store tag = store.Format.mark_open_stag tag
-let set_meta ppf store ~style_renderer =
-  let meta = function
-    | Hxd_style_renderer -> style_renderer
-    | _ -> "Hxd: god, we broken everythings" in
-  let store = { store with Format.mark_open_stag= meta } in
-  set_meta_store ppf store
 
 let style_renderer ppf =
   let res = meta_raw (meta_store ppf) style_renderer_tag in
